fceffacd6c2d3258ecc72ecd6f2163b431d11c8c
Bikash Narayan Panda <oksbwn@gmail.com>
2017-05-13 03:05:23 +0800

    Added GPIO Pin Mapping according to NP4J and added support for all pins as GPIO/ Digital

--- src/in/weargenius/gpio/NanoPiPin.java
+++ src/in/weargenius/gpio/NanoPiPin.java
@@ -1,21 +1,32 @@
 package in.weargenius.gpio;
 
 public class NanoPiPin {
-	public static int GPIO_00=104;
-	public static int GPIO_01=0;
-	public static int GPIO_02=0;
-	public static int GPIO_03=0;
-	public static int GPIO_04=0;
-	public static int GPIO_05=0;
-	public static int GPIO_06=0;
-	public static int GPIO_07=0;
-	public static int GPIO_08=0;
-	public static int GPIO_09=0;
-	public static int GPIO_10=0;
-	public static int GPIO_11=0;
-	public static int GPIO_12=0;
-	public static int GPIO_13=0;
-	public static int GPIO_14=0;
-	public static int GPIO_15=0;
-
+	public static int GPIO_08=98;
+	public static int GPIO_09=99;
+	public static int GPIO_07=104;
+	public static int GPIO_00=61;
+	public static int GPIO_02=62;
+	public static int GPIO_03=63;
+	public static int GPIO_12=95;
+	public static int GPIO_13=96;
+	public static int GPIO_14=93;
+	public static int GPIO_30=101;
+	public static int GPIO_21=72;
+	public static int GPIO_22=71;
+	public static int GPIO_23=77;
+	public static int GPIO_24=75;
+	public static int GPIO_25=163;
+	public static int GPIO_15=117;
+	public static int GPIO_16=113;
+	public static int GPIO_01=97;
+	public static int GPIO_04=78;
+	public static int GPIO_05=69;
+	public static int GPIO_06=60;
+	public static int GPIO_10=94;
+	public static int GPIO_11=58;
+	public static int GPIO_31=100;
+	public static int GPIO_26=92;
+	public static int GPIO_27=74;
+	public static int GPIO_28=76;
+	public static int GPIO_29=73;
 }
